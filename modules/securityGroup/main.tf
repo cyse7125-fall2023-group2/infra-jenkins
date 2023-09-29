39ba8b59e6bfab1241b800e2411d259527d69e8f
Ankit <ankitlall@Ankits-MacBook-Pro.local>
2023-09-29 16:04:14 -0400

    caddy config

--- modules/securityGroup/main.tf
+++ modules/securityGroup/main.tf
@@ -11,6 +11,14 @@ resource "aws_security_group" "application" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+    ingress {
+    description = "HTTPS ingress"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     description = "SSH ingress"
     from_port   = 22
